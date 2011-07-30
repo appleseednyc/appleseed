ed186a72aac26226d196bdff1c40d60db21a2dad
David H <david@haystackproject.com>
2011-07-30 16:13:02 -0400

    Folder for iPad fun.

---
+++ ipad/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<!--
+    
+-->
+<head>
+  <title>NYC.gov for iPad</title>
+  <meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
+  <meta name="apple-mobile-web-app-capable" content="yes" />
+  <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+  <meta name="viewport" content="user-scalable=no, width=device-width" />
+  <script type="text/javascript">
+    function BlockMove(event) {
+      // Tell Safari not to move the window.
+      event.preventDefault() ;
+    }
+  </script>
+</head>
+<body ontouchmove="BlockMove(event);">
+    Yo dawg, I heard you liek iPads.
+</body>
+<script type="text/javascript">
+   
+</script>
+</html>
